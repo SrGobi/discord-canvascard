82cb8c27ff7679642af81b666fb2bfa61a3490bc
SrGobi <62013924+SrGobi@users.noreply.github.com>
2021-05-15 19:40:28 +0200

    Error de fuente solucionado

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ Canvas.registerFont(`${__dirname}/assets/fonts/SketchMatch.ttf`, { family: "Sket
 // Registrar fuente SketchMatch
 Canvas.registerFont(`${__dirname}/assets/fonts/LuckiestGuy-Regular.ttf`, { family: "luckiest guy" });
 // Registrar fuente Burbank Big Condensed Black
-Canvas.registerFont(`${__dirname}/assets/fonts/Burbank Big Condensed-Black-700.ttf`, { family: "file0" });
+Canvas.registerFont(`${__dirname}/assets/fonts/Burbank Big-Condensed-Black-700.ttf`, { family: "file0" });
 // Registrar fuente Roboto Black
 Canvas.registerFont(`${__dirname}/assets/fonts/Roboto-Black.ttf`, { family: "Roboto Black" });
 // Registrar fuente Roboto
@@ -15,4 +15,3 @@ Canvas.registerFont(`${__dirname}/assets/fonts/Roboto-Regular.ttf`, { family: "R
 module.exports.Base = require('./src/greetings/Base');
 module.exports.FortniteShop = require('./src/fortnite/Shop');
 module.exports.FortniteStats = require('./src/fortnite/Stats');
-module.exports.FortniteEvents = require('./src/fortnite/Events');
